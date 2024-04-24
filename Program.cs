fb1701a793ed3d28face715b0ed8ed8a8358caca
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-24 20:29:29 +0100

    Added name verofication, and welcome.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ int playerHP = 10;
 int playerArmor = 10;
 int playerAttackLevel = 5;
 int playerAttackDamage = 10;
-decimal armorMultiplier = 0.2d;
+decimal armorMultiplier = 0.2M;
 string playerName = "";
 
 Console.WriteLine("Welcome to txtFight, a small text based RPG game");
@@ -15,7 +15,20 @@ Console.WriteLine("Can you please enter your players name:");
 bool flag = true;
 do {
 playerName = Console.ReadLine();
-Console.WriteLine($"You entered {playerName} for your name. Is this correct?");
+Console.WriteLine($"You entered {playerName} for your name. Is this correct?\nEnter [1] for Yes \t Enter [2] for No");
+var isCorrectAnswer = Console.ReadLine();
+if (isCorrectAnswer == "1") {
+    flag = false;
+Console.WriteLine($"Hello, {playerName} pleasure to meet you. Let's introduce you to your character.");
+Console.WriteLine($"HP\t\tArmour\t\tAttack Level\t\tAttack DMG\t\tArmour Multi");
+Console.WriteLine($"{playerHP}\t\t{playerArmor}\t\t{playerAttackLevel}\t\t{playerAttackDamage}\t\t{armorMultiplier}");
+Console.ReadLine();
+
+}
+else
+{
+    Console.WriteLine("Please re-enter your name.");
+}
 
 }
 while (flag == true);
